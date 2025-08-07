53d6de2d2bb7d15a38d11947db12205427ab2cd0
TanJiRo-AkaZa <viratkarthikskingcoder123@gmail.com>
2025-08-07 21:28:41 +0530

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,21 +1,11 @@
-unit=int(input("please enter the number of units entered"))
-amount=0
-extra=0
-if  unit< 50 :
- amount=unit*2.60
- extra=25
-elif unit<=100:
- amount=130 +((unit-50)*3.25)
- extra = 35
-
-elif unit<=200:
- amount=130+162.5+((unit-100)*5.26)
- extra=45
-
-else :
-  amount=130+162.5+526+((unit-200)*8.45)
-  extra=75
-
-total=amount+ extra
-print("\nElectricity Bill =%.2f" %total)
-
+num=int(input("enter a number"))
+sum=0
+temp=num
+while temp>0:
+    digit=temp%10
+    sum += digit**3
+    temp//=10
+if num==sum:
+    print("it is a armstrong number")
+else:
+    print("it is not a armstrong number")

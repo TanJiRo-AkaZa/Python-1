3fe55afdf8c61e661d2878e939d2db6d48412b30
TanJiRo-AkaZa <viratkarthikskingcoder123@gmail.com>
2025-08-23 20:10:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-string = input("please enter your own word")
-char=input("please enter your own character")
-i=0
-count=0
-while(i<len(string)):
-    if(string[i]==char):
-        count = count+1
-    i= i + 1
-print("the total no of times", char,"has occured=",count)
+def well_wishes():
+    print("hello" )
+    print("how are you")
+
+well_wishes()

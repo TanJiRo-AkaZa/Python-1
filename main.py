11e4c977e375cfc8b0da11c9954ff1e4ffeecb6b
TanJiRo-AkaZa <viratkarthikskingcoder123@gmail.com>
2025-08-29 20:37:47 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def well_wishes():
-    print("hello" )
-    print("how are you")
+def total_calc(bill_amount,tip_perc=10):
+    total=bill_amount*(1+0.01*tip_perc)
+    total=round(total,2)
+    print(f"please pay ${total}")
 
-well_wishes()
+total_calc(150,20)
+total_calc(200)

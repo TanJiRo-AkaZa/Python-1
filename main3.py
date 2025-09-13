177422e442f42b3c00ae51e2f14fb768662a762c
TanJiRo-AkaZa <viratkarthikskingcoder123@gmail.com>
2025-09-13 20:38:34 +0530

    Add files via upload

--- main3.py
+++ main3.py
@@ -1,28 +1,11 @@
-def hotel(nights):
-    return nights * 140
-def plane(city):
-    if city == "London":
-        return 183
-    elif city == "Paris":
-        return 220
-    elif city == "Berlin":
-        return 222
-    elif city == "Madrid":
-        return 475
-def car(days):
-    if days>=7:
-        return days*40-50
-    elif days>=3:
-        return days*40-20
-    else:
-        return days*40
-def trip_cost(city, days, spending_money):
-    return car(days)+hotel(days)+plane(city)+spending_money
-print("cost of rental car", car(5))
-print("the cost of plane ride", plane("Paris"))
-print("cost of hotel", hotel(5))
-
-print("total trip cost", trip_cost("London", 5, 200))
-print(trip_cost("Paris", 5, 200))
-
-
+l=[4,5,1,2,9,7,10,8]
+print("the original list is:",l)
+count=0
+for i in l:
+    count+=i
+avg=count/len(l)
+print("sum=",count)
+print("average=",avg)
+l.sort()
+print("the smallest number is",l[0])
+print("the largest number is",l[-1])

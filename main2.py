41165a0b458090575a1952b1bdd30d5ae11f8a99
TanJiRo-AkaZa <viratkarthikskingcoder123@gmail.com>
2025-10-18 20:29:39 +0530

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,13 +1,16 @@
-class employee:
-    def __init__(self):
-        print("employee created")
-    def __del__(self):
-        print("detructor called, employee deleted.")
-def Create__obj():
-    print("making object")
-    obj = employee()
-    print("function end")
-    return obj
-print("calling Create__obj function....")
-obj = Create__obj()
-print("program end")
+class person(object):
+    def __init__(self,name,idnumber):
+       self.name=name
+       self.idnumber=idnumber
+    def display(self):
+        print("name:",self.name)
+        print("ID number:",self.idnumber)   
+class employee(person):
+        def __init__(   self,name,idnumber,salary,post):
+            
+            self.salary=salary
+            self.post=post
+            person.__init__(self,name,idnumber)
+a=employee("rahul",886012,2000000,"manager")
+a.display()
+    

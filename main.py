473a1d8302b09f0e29f311f3f7f4c55c0fd01811
TanJiRo-AkaZa <viratkarthikskingcoder123@gmail.com>
2025-10-24 20:32:16 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-class veichle:
-    def __init__(self,name,maxspeed,mileage):
-       self.name=name
-       self.maxspeed=maxspeed
-       self.mileage=mileage
-class bus(veichle):
-     pass
-school_bus=bus("School Volvo",180,12)
-print("veichle name:",school_bus.name,"speed:",school_bus.maxspeed,"mileage:",school_bus.mileage    )
+class myclass:
+    __privatvar = 27
+    def __privatmet(self):
+        print("Iam inside class of myclass")
+    def hello(self):
+        print("private variable value is:",myclass.__privatvar)
+foo =myclass()
+foo.hello()
+foo.__privatmet()  # This will raise an AttributeError

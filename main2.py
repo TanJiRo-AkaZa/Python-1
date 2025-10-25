a894d2a6f2cc38b5f4edcc9f117b6c185aa27128
TanJiRo-AkaZa <viratkarthikskingcoder123@gmail.com>
2025-10-25 20:28:01 +0530

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,11 +1,20 @@
-class computer:
-    def __init__(self):
-        self.max_price = 900
-    def sell(self):
-        print("Selling Price: {}".format(self.max_price))
-    def setmaxprice(self, price):
-        self.max_price = price
-c = computer()
-c.sell()
-c.setmaxprice(2000)
-c.sell()
+class India():
+    def capital(self):
+        print("New Delhi is the capital of India")
+    def language(self):
+        print("Hindi is the most widely spoken language of India")
+    def type(self):
+        print("India is a developing country")
+class USA():
+    def capital(self):
+        print("Washington, D.C. is the capital of USA" )
+    def language(self):
+        print("English is the primary language of USA")
+    def type(self):
+        print("USA is a developed country")
+obj_ind = India()
+obj_usa = USA()
+for country in (obj_ind, obj_usa):
+    country.capital()
+    country.language()
+    country.type()

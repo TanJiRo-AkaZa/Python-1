17139c62fbf94bf2b7094a708cc6879b325acfc0
TanJiRo-AkaZa <viratkarthikskingcoder123@gmail.com>
2025-11-07 21:09:15 +0530

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,20 +1,31 @@
-class India():
-    def capital(self):
-        print("New Delhi is the capital of India")
-    def language(self):
-        print("Hindi is the most widely spoken language of India")
-    def type(self):
-        print("India is a developing country")
-class USA():
-    def capital(self):
-        print("Washington, D.C. is the capital of USA" )
-    def language(self):
-        print("English is the primary language of USA")
-    def type(self):
-        print("USA is a developed country")
-obj_ind = India()
-obj_usa = USA()
-for country in (obj_ind, obj_usa):
-    country.capital()
-    country.language()
-    country.type()
+import pygame
+pygame.init()
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+backgroundimg = pygame.transform.scale(pygame.image.load("bg.jpg").convert(), (SCREEN_WIDTH, SCREEN_HEIGHT))
+isagiimg =pygame.transform.scale(pygame.image.load("images.jpg").convert_alpha(), (200, 200))
+isagirect = isagiimg.get_rect(center =(SCREEN_WIDTH//2, SCREEN_HEIGHT//2 -30))
+text = pygame.font.Font(None, 36).render("Isagi", True,pygame.Color("black"))
+textrect = text.get_rect(center =(SCREEN_WIDTH//2, SCREEN_HEIGHT +110))
+
+def game_loop():
+                 
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.blit(backgroundimg, (0, 0))
+        screen.blit(isagiimg, isagirect)
+        screen.blit(text, textrect)
+        pygame.display.flip()
+        clock.tick(30)
+    pygame.quit()
+if __name__ == "__main__":
+    game_loop()
+ 
+            
+        
+        
+        

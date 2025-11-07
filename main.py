51e5599c1dd9fea38f7026a1ce0c66a163bd5791
TanJiRo-AkaZa <viratkarthikskingcoder123@gmail.com>
2025-11-07 20:35:46 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-class A:
-    def __init__(self,a):
-        self.a = a
-    def __lt__(self,other):
-        if (self.a < other.a):
-            return "ob 1 is less than obj 2"   
-        else:
-            return "ob 1 is not less than obj 2"
-    def __eq__(self,other):
-        if (self.a == other.a):
-            return "obj 1 is equal to obj 2"   
-        else:
-            return "obj 1 is not equal to obj 2"
-ob1 = A(5)
-ob2 = A(10)
-print(ob1 < ob2)
-ob3 = A(3)
-ob4 = A(3)
-print(ob3 == ob4)
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((400 , 500))
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.flip()

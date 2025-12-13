f395fddfb01ed108803a2ac314b94243896cdc7e
TanJiRo-AkaZa <viratkarthikskingcoder123@gmail.com>
2025-12-13 20:38:42 +0530

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,31 +1,9 @@
-import pygame
-pygame.init()
-SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-backgroundimg = pygame.transform.scale(pygame.image.load("images.jpg").convert(), (SCREEN_WIDTH, SCREEN_HEIGHT))
-isagiimg =pygame.transform.scale(pygame.image.load("sl.jpg").convert_alpha(), (200, 200))
-isagirect = isagiimg.get_rect(center =(SCREEN_WIDTH//2, SCREEN_HEIGHT//2 -30))
-text = pygame.font.Font(None, 36).render("Isagi", True,pygame.Color("black"))
-textrect = text.get_rect(center =(SCREEN_WIDTH//2, SCREEN_HEIGHT +110))
-
-def game_loop():
-                 
-    clock = pygame.time.Clock()
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        screen.blit(backgroundimg, (0, 0))
-        screen.blit(isagiimg, isagirect)
-        screen.blit(text, textrect)
-        pygame.display.flip()
-        clock.tick(30)
-    pygame.quit()
-if __name__ == "__main__":
-    game_loop()
- 
-            
-        
-        
-        
+import tkinter as tk
+from tkinter import messagebox
+root = tk.Tk()
+root.geometry("400x300")
+def msg():
+    messagebox.showwarning("alert", "stop virus is visible")
+button = tk.Button(root, text="Click Me", command=msg)
+button.place(x=40,y=80)
+root.mainloop()
